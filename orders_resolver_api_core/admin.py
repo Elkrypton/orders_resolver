e71a6a771b37f79b62261420799493d2dfbfa08b
Rochdi El Majdoub <36573322+Elkrypton@users.noreply.github.com>
2024-03-29 13:29:01 +0000

    ..

--- orders_resolver_api_core/admin.py
+++ orders_resolver_api_core/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import *
+
+
+admin.site.register(Order)
+admin.site.register(Vendor)
+admin.site.register(Retail)
+admin.site.register(Customer)
+admin.site.register(Product)
