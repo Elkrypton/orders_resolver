405207e02982df0878de54aafef7b0dbbaf3f085
Rochdi El Majdoub <36573322+Elkrypton@users.noreply.github.com>
2024-03-29 21:18:43 -0400

    registering models to admin interface

--- orders_resolver_api_core/admin.py
+++ orders_resolver_api_core/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+
+admin.register(Order)
+admin.register(Product)
+admin.register(Vendor)
+admin.register(Retail)
+admin.register(Issue)
+admin.register(Customer)
