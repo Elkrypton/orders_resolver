c24152fdcb2bc7bcdcfb655dd13892d5d3e5b709
Rochdi El Majdoub <36573322+Elkrypton@users.noreply.github.com>
2024-05-21 22:46:10 -0400

    adding throttling mechanism to API (Rate Limit per user/non-users

--- orders_resolver_api/settings.py
+++ orders_resolver_api/settings.py
@@ -57,6 +57,14 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
         
     ],
+	'DEFAULT_THROTTLE_CLASSES': [
+		'rest_framework.throttling.AnonRateThrottle',
+		'rest_framework.throttling.UserRateThrottle',
+    ],
+	'DEFAULT_THROTTLE_RATES': {
+		'anon': '100/day',
+		'user': '1000/day',
+    }
 
 }
 
