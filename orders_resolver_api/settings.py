78de842ec2dc52dc9cb2ab16d5e8cb54183d285e
Rochdi El Majdoub <36573322+Elkrypton@users.noreply.github.com>
2024-05-21 22:41:08 -0400

    adding dotenv mechanism

--- orders_resolver_api/settings.py
+++ orders_resolver_api/settings.py
@@ -12,8 +12,16 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+import dotenv
+
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+dotenv_file = os.path.join(BASE_DIR, '.env')
+if os.path.isfile(dotenv_file):
+	dotenv.read_dotenv(dotenv_file)
 
 
 # Quick-start development settings - unsuitable for production
